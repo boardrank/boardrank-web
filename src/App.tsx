57d3c7ccb2580232d5735e5131c330bc65001ea8
yongho <wjddydgh94@naver.com>
2021-12-26 12:49:26 +0900

    Feat: login 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import Header from "components/Header";
 import AppRouter from "Router";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
-
 import "./styles/css/reset.css";
 import "./styles/css/style.css";
 import styled from "styled-components";
+import useUser from "hooks/useUser";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const { userObj } = useUser();
+
+  const LoginCheck = () => {
+    userObj ? setIsLoggedIn(true) : setIsLoggedIn(false);
+  };
+
+  useEffect(() => {
+    LoginCheck();
+  }, [userObj]);
+
   return (
     <Router>
-      <Header />
+      <Header isLoggedIn={isLoggedIn} />
       <AppLayout>
         <AppRouter />
       </AppLayout>
